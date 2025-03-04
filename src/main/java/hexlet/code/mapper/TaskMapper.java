5224517656bf8c56766332c62e625bc5376bf8ee
Nikita Ryazanov <ryazanov.na@yandex.ru>
2025-03-04 15:00:21 +0400

    исправления к четвертому комментарию 2

--- src/main/java/hexlet/code/mapper/TaskMapper.java
+++ src/main/java/hexlet/code/mapper/TaskMapper.java
@@ -22,7 +22,7 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 @Mapper(
-        uses = { JsonNullableMapper.class },
+        uses = { JsonNullableMapper.class, ReferenceMapper.class },
         nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE,
         componentModel = MappingConstants.ComponentModel.SPRING,
         unmappedTargetPolicy = ReportingPolicy.IGNORE
--- src/main/java/hexlet/code/mapper/UserMapper.java
+++ src/main/java/hexlet/code/mapper/UserMapper.java
@@ -15,7 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Mapper(
-        uses = { JsonNullableMapper.class },
+        uses = { JsonNullableMapper.class, ReferenceMapper.class },
         nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE,
         componentModel = MappingConstants.ComponentModel.SPRING,
         unmappedTargetPolicy = ReportingPolicy.IGNORE
